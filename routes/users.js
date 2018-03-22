5b30a15d368d4e33d9ec68e27efd07ccddf5cffa
Lee Brandt <leebrandt@gmail.com>
2018-03-22 15:09:45 -0500

    Updated Node app with Postgres

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {Client} = require('pg');
+const client = new Client({connectionString:process.env.DATABASE_URL});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  client.connect();
+  client.query('SELECT * FROM "Speakers"', (err, rsp) =>{
+    if(err) throw new Error(err);
+    res.status(200).send(rsp.rows);
+    client.end();
+  })
 });
 
 module.exports = router;
